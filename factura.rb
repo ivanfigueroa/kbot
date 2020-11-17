b86481acf99be19ee53e942aaabdff8e2a684434
mario figueroa <mysw1@hotmail.com>
2020-11-17 23:27:57 +0000

    create factura class

--- factura.rb
+++ factura.rb
@@ -1,1 +1,12 @@
-puts "hello world!"
+class Factura
+    def initialize()
+    end
+
+    def calculate ()
+        return "Hola, Mundo" 
+    end
+end
+
+
+invoice_bot = Factura.new()
+puts invoice_bot.calculate()
